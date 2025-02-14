f9ed9aa744e8bb633753ac17bf0fcfd9902fabbe
huynh_systemi <nguyen.huu.huy@systemi.co.jp>
2025-02-14 11:25:23 +0700

    1

--- src/app.service.ts
+++ src/app.service.ts
@@ -3,6 +3,6 @@ import { Injectable } from '@nestjs/common';
 @Injectable()
 export class AppService {
   getHello() {
-    return { msg: `Hello world ! ${process.env.NODE_ENV}` };
+    return { msg: `Hello world test ! ${process.env.NODE_ENV}` };
   }
 }
