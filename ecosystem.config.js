11c38b4192bbb76a342ef9e4bb7a664a91f5c2a4
huynh_systemi <nguyen.huu.huy@systemi.co.jp>
2025-02-14 11:40:36 +0700

    1

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -65,8 +65,12 @@ module.exports = {
       repo: 'git@github.com:skydark3004/training-devops-be.git', // Repository Git.
       path: '/home/thangl-vietis/deploy-pm2', // Thư mục trên server.
       'pre-deploy-local': "echo 'Deploying to production server'", // Script chạy trên máy local trước khi deploy.
+      'pre-deploy':
+        'export NVM_DIR="$HOME/.nvm" && [ -s "$NVM_DIR/nvm.sh" ] && . "$NVM_DIR/nvm.sh" && cd /home/thangl-vietis/deploy-pm2 && npm instal && npm run build && pm2 startOrRestart ecosystem.config.js --env development', // Script chạy trên server sau khi deploy.
       'post-deploy':
-        'export NVM_DIR="$HOME/.nvm" && [ -s "$NVM_DIR/nvm.sh" ] && . "$NVM_DIR/nvm.sh" && cd /home/thangl-vietis/deploy-pm2/current && npm instal && npm run build && pm2 startOrRestart ecosystem.config.js --env development', // Script chạy trên server sau khi deploy.
+        'export NVM_DIR="$HOME/.nvm" && [ -s "$NVM_DIR/nvm.sh" ] && . "$NVM_DIR/nvm.sh" && cd /home/thangl-vietis/deploy-pm2 && npm instal && npm run build && pm2 startOrRestart ecosystem.config.js --env development', // Script chạy trên server sau khi deploy.
+      /*       'pre-deploy':
+      
       /*       'pre-deploy':
         'export NVM_DIR="$HOME/.nvm" && [ -s "$NVM_DIR/nvm.sh" ] && . "$NVM_DIR/nvm.sh" && cd /home/thangl-vietis/deploy-pm2/current; npm install; npm run build', */
     },
