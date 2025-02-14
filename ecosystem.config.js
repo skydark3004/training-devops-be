26d068766f088a8ce2ebc9123743fe6d6ab83ac5
huynh_systemi <nguyen.huu.huy@systemi.co.jp>
2025-02-14 11:43:14 +0700

    1

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -65,7 +65,7 @@ module.exports = {
       repo: 'git@github.com:skydark3004/training-devops-be.git', // Repository Git.
       path: '/home/thangl-vietis/deploy-pm2', // Thư mục trên server.
       'pre-deploy-local': "echo 'Deploying to production server'", // Script chạy trên máy local trước khi deploy.
-
+      'pre-deploy': 'pwd',
       'post-deploy':
         'export NVM_DIR="$HOME/.nvm" && [ -s "$NVM_DIR/nvm.sh" ] && . "$NVM_DIR/nvm.sh" && pm2 startOrRestart ecosystem.config.js --env development', // Script chạy trên server sau khi deploy.
       /*       'pre-deploy':
