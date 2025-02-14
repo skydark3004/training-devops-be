ffeb23f0136a3ad290785f815156a921c62f7756
huynh_systemi <nguyen.huu.huy@systemi.co.jp>
2025-02-14 11:33:58 +0700

    1

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -66,9 +66,9 @@ module.exports = {
       path: '/home/thangl-vietis/deploy-pm2', // Thư mục trên server.
       'pre-deploy-local': "echo 'Deploying to production server'", // Script chạy trên máy local trước khi deploy.
       'post-deploy':
-        'export NVM_DIR="$HOME/.nvm" && [ -s "$NVM_DIR/nvm.sh" ] && . "$NVM_DIR/nvm.sh" && cd /home/thangl-vietis/deploy-pm2/current && pm2 restart ecosystem.config.js --env development', // Script chạy trên server sau khi deploy.
-      'pre-deploy':
-        'export NVM_DIR="$HOME/.nvm" && [ -s "$NVM_DIR/nvm.sh" ] && . "$NVM_DIR/nvm.sh" && cd /home/thangl-vietis/deploy-pm2/current; npm install; npm run build',
+        'export NVM_DIR="$HOME/.nvm" && [ -s "$NVM_DIR/nvm.sh" ] && . "$NVM_DIR/nvm.sh" && cd /home/thangl-vietis/deploy-pm2/current && npm instal && npm run build && pm2 restart ecosystem.config.js --env development', // Script chạy trên server sau khi deploy.
+      /*       'pre-deploy':
+        'export NVM_DIR="$HOME/.nvm" && [ -s "$NVM_DIR/nvm.sh" ] && . "$NVM_DIR/nvm.sh" && cd /home/thangl-vietis/deploy-pm2/current; npm install; npm run build', */
     },
   },
 };
