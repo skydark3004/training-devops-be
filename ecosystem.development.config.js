05539aa3621ac6d32c63a6b762c5fda3b3343364
huynh_systemi <nguyen.huu.huy@systemi.co.jp>
2025-02-18 10:53:14 +0700

    1

--- ecosystem.development.config.js
+++ ecosystem.development.config.js
@@ -44,4 +44,15 @@ module.exports = {
       time: true, // Show time on the console
     },
   ],
+  deploy: {
+    development: {
+      user: 'thangl-vietis', // user to ssh
+      host: ['34.124.149.226'], // IP server.
+      ref: 'origin/main', // Branch Git to deploy
+      repo: 'git@github.com:skydark3004/training-devops-be.git', // Repository Git.
+      path: '/home/thangl-vietis/deploy-pm2', // directory to folder pm2 deploy
+      'pre-deploy-local': "echo 'Deploying to development server...'",
+      'post-deploy': `sh deploy.sh development`,
+    },
+  },
 };
